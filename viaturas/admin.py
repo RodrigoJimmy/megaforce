93c72d27d928cf2c47f4f6482f6da4c8f31a61a5
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2017-04-12 23:55:39 -0300

    N+1 queries
    - Resolve o problema de  N+1 consultas no banco através do
    list_select_related
    - placa agora é clicável pra acessar o modelo viatura.
    - Muda nome do site admin para MegaForce.

--- viaturas/admin.py
+++ viaturas/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Marca, Modelo, Viatura
 
+admin.AdminSite.site_header = "MegaForce"
+admin.AdminSite.site_title = "MegaForce site admin"
+
 
 @admin.register(Marca)
 class MarcaAdmin(admin.ModelAdmin):
@@ -11,10 +14,14 @@ class MarcaAdmin(admin.ModelAdmin):
 @admin.register(Modelo)
 class ModeloAdmin(admin.ModelAdmin):
     list_display = ('nome', 'marca')
+    list_select_related = ('marca',)
 
 
 @admin.register(Viatura)
 class ViaturaAdmin(admin.ModelAdmin):
     list_display = ('prefixo', 'modelo', 'status', 'placa', 'updated_at')
+    list_display_links = ('prefixo', 'placa')
     list_filter = ('status', 'modelo', 'updated_at')
     search_fields = ('prefixo', 'placa')
+    list_select_related = ('modelo',)
+
